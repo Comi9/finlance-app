438ffb86d21ff7ed97816e9446d55cc4af6a5a40
Cosmin <cosminp9@gmail.com>
2020-11-27 10:13:24 +0200

    add tailwindcss, use tailwindui

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import '../styles/globals.css'
+import '../styles/styles.css'
 
 function FINlanceApp({ Component, pageProps }) {
   return <Component {...pageProps} />
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [
+    './src/**/*.html',
+    './src/**/*.js',
+  ],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    // require('@tailwindcss/custom-forms')
+  ],
+}
